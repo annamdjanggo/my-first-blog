804fb780a283f88b7c79d95a4bef7a3175ea1cc9
Your Name <your@example.com>
2017-08-12 15:41:08 -0700

    We fixed it

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'te1=7ae6t55s+!--=+*df%trqfjaep-a=75^kw$*k&9x37w&w^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'annamdjanggo.pythonanywhere.com']
 
 
 # Application definition
